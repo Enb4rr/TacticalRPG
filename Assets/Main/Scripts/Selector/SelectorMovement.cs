1c28934951436be2189bdc363b40704b15cc4cbe
Julian Rosero Narvaez <jrosero@payday.local>
2023-07-10 16:56:46 -0500

    Working on camera and selector stuff

---
+++ Assets/Main/Scripts/Selector/SelectorMovement.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SelectorMovement : MonoBehaviour
+{
+    [SerializeField] private Vector3 movementVector;
+}
